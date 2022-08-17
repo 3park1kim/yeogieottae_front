95c0f4e7e4081f448a79fafb439ddd70364e353f
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-08-17 23:05:13 +0900

    :fire: [#3] feat: 카카오 로그인 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
+const { Kakao } = window;
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
 function App() {
-  return <div className="App">안녕</div>;
+  const loginWithKakao = () => {
+    Kakao.Auth.authorize({
+      redirectUri: 'http://localhost:3000',
+    })
+  }
+
+  return (
+    <div className="App">
+      <a id="custom-login-btn" onClick={loginWithKakao}>
+        <img
+          src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+          width="222"
+          alt="카카오 로그인 버튼"
+        />
+      </a>
+    </div>
+  )
+
 }
 
 export default App;
