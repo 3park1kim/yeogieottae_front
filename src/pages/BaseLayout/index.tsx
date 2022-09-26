021887bf55bb8f7b5b8fc632171556475d32af8a
parkyw1206 <36143152+parkyw1206@users.noreply.github.com>
2022-09-26 09:02:51 +0900

    [#3] feat: 기본 Routing 추가 및 Mate Page 추가

--- src/pages/BaseLayout/index.tsx
+++ src/pages/BaseLayout/index.tsx
@@ -1,14 +1,38 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import HeartIcon from "../../shared/Icons/HeartIcon";
+import Mate from "../Mate";
 import "./index.scss";
 const BaseLayout = () => {
+  const [menu, changeMenu] = useState("home");
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate(menu);
+  }, [menu]);
   return (
     <div className="layout">
       <div className="side">
-        <div className="side-top">상단</div>
-        <div className="side-bottom">메인 하단</div>
+        <div className="side-top">
+          상단
+          <button onClick={() => changeMenu("/home")}>홈</button>
+          <button onClick={() => changeMenu("/mate")}>메이트</button>
+          <button onClick={() => changeMenu("/me")}>나</button>
+        </div>
+        <div className="side-bottom">
+          {menu === "/mate" && (
+            <div>
+              <HeartIcon width={30} height={30} />
+            </div>
+          )}
+        </div>
       </div>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/login`} element={<div />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/home`}
+          element={<div>home</div>}
+        />
+        <Route path={`${process.env.PUBLIC_URL}/mate`} element={<Mate />} />
+        <Route path={`${process.env.PUBLIC_URL}/me`} element={<div>Me</div>} />
         <Route path={`${process.env.PUBLIC_URL}/*`} element={<div>바디</div>} />
       </Routes>
     </div>
---
+++ src/pages/Mate/index.tsx
@@ -0,0 +1,7 @@
+const { Kakao } = window;
+
+const Mate: React.FC = () => {
+  return <div>mate</div>;
+};
+
+export default Mate;
